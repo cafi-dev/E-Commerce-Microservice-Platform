6cd2dfed6b0d191ed94fa8c4317e5b3d60949ba1
iacgen.appcd <131802650+iacgen@users.noreply.github.com>
2023-05-31 17:15:56 +0530

    Added file appCD/modules/rds/variables.tf

---
+++ appCD/modules/rds/variables.tf
@@ -0,0 +1,31 @@
+variable "project" {
+
+}
+
+variable "env" {
+
+}
+
+variable "vpc_id" {
+
+}
+
+variable "private_subnets" {
+
+}
+
+variable "cidr_blocks" {
+
+}
+
+variable "db_name" {
+
+}
+
+variable "db_username" {
+
+}
+
+variable "db_password" {
+
+}

fc2dabba0edfa428263fe1ab623a9b76e04e49a4
iacgen.appcd <131802650+iacgen@users.noreply.github.com>
2023-05-31 17:15:54 +0530

    Added file appCD/modules/rds/outputs.tf

---
+++ appCD/modules/rds/outputs.tf
@@ -0,0 +1,39 @@
+output "db_instance_address" {
+    value = module.db.db_instance_address
+    description = "The address of the RDS instance"
+}
+
+output "db_instance_arn" {
+    value = module.db.db_instance_arn
+    description = "The ARN of the RDS instance"
+}
+
+output "db_instance_endpoint" {
+    value = module.db.db_instance_endpoint
+    description = "The connection endpoint"
+}
+
+output "db_instance_engine" {
+    value = module.db.db_instance_engine
+    description = "The database engine"
+}
+
+output "db_instance_id" {
+    value = module.db.db_instance_id
+    description = "The RDS instance ID"
+}
+
+output "db_instance_name" {
+    value = module.db.db_instance_name
+    description = "The database name"
+}
+
+output "db_instance_password" {
+    value = module.db.db_instance_password
+    description = "The database password (this password may be old, because Terraform doesn't track it after initial creation)"
+}
+
+output "db_instance_port" {
+    value = module.db.db_instance_port
+    description = "The database port"
+}

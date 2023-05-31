58503cf92b5f8f019e3b2de4f7d59f66f6ee9d59
iacgen.appcd <131802650+iacgen@users.noreply.github.com>
2023-05-31 17:16:00 +0530

    Added file appCD/rds.tf

---
+++ appCD/rds.tf
@@ -0,0 +1,11 @@
+module "rds" {
+    project = var.project
+    env = var.env
+    vpc_id = module.vpc.id
+    cidr_blocks = [var.cidr_block]
+    private_subnets = module.vpc.private_subnet_ids
+
+    # db details
+    db_username = var.db_username
+    db_password = var.db_password
+}
